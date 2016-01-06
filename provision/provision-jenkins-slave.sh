285d530e52a414d359c5f4c059ac625a8dd97098
James Sharpe <james.sharpe@zenotech.com>
2016-01-06 14:53:51 +0000

    Install ansible via apt

--- provision/provision-jenkins-slave.sh
+++ provision/provision-jenkins-slave.sh
@@ -2,14 +2,12 @@
 
 PROVISION_REPO=https://github.com/zenotech/zProvision.git
 
-sudo -H apt-get -y install python-pip python-dev build-essential
-sudo -H pip install --upgrade pip
-sudo -H pip install ansible
+sudo -H apt-get -y install ansible
 
 git clone ${PROVISION_REPO}
 
 cd zProvision/ansible
-sudo -H ./setup.sh #Install dependencies
+sudo -H sh ./setup.sh #Install dependencies
 echo "[ci-slave]
 localhost ansible_connection=local
 " > host_inventory
